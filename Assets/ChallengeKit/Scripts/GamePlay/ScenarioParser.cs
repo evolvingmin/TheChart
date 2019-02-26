c93716665e591eccca1efa61882e08a787028d75
evolvingmin <evolvingmin@live.co.kr>
2019-02-26 21:46:12 +0900

    ADD HUGE Change!  - 2DGameKit For check improvement my ChallengeKit code base  - Based on this Resource, I'll make 2D Game What I have in mind

---
+++ Assets/ChallengeKit/Scripts/GamePlay/ScenarioParser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ChallengeKit.Pattern;
+
+namespace ChallengeKit.GamePlay
+{
+    class ScenarioParser : IParser
+    {
+        ScenarioSystem scenarioSystem;
+
+        public Define.Result Init(SystemMono parentSystem)
+        {
+            scenarioSystem = (ScenarioSystem)parentSystem;
+
+            if (scenarioSystem == null)
+                return Define.Result.NOT_INITIALIZED;
+
+            return Define.Result.OK;
+        }
+
+        public void ParseCommand(string Command, params object[] Objs)
+        {
+            if (Command == "OnSelectionConfirm")
+            {
+                scenarioSystem.UpdateScenarioNode((int)Objs[0]);
+            }
+        }
+    }
+}
